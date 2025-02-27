bd3f2b3286aa2e648a303baf9533aabe6764da13
kapibaba <jksuan@163.com>
2025-02-27 16:50:03 +0800

    Screenshots showing desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -45,8 +45,8 @@ export default function Page() {
 						src="/hero-mobile.png"
 						width={560}
 						height={620}
-						className="block md:block"
-						alt="Screenshots of the dashboard project showing desktop version"
+						className="block md:hidden"
+						alt="Screenshots of the dashboard project showing mobile version"
 					/>
 				</div>
 			</div>
